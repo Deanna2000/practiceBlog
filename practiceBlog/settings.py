17576fe378d4492b258fa28a5588420ecda6224c
Deanna Vickers <deanna2000@gmail.com>
2018-07-19 11:30:07 -0500

    set up project skeleton

--- practiceBlog/settings.py
+++ practiceBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qvu)nbm%$jryo&x-3a(@kyzj&!4(@_^gk6s&--$dsm+xa05+p&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'CST6CDT'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
